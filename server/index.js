d1b2a47ac7d3695f54d932bbefb6dc6ab2374d0f
Devarsh Panchal <devarsh.panchal@rapidops.com>
2022-04-11 10:31:30 +0530

    Updated server/index.js: Added path.resolve

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 app.use(morgan("tiny"));
 
-app.use(express.static("../client/build"));
+app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
 });
